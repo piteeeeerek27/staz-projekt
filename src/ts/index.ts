4074ebcb7f57974a2cf5796371d959f7b11a17e2
Piotr Rześny <piotr.rzesny@billennium.com>
2022-03-08 10:30:04 +0100

    toggle sidebar

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -10,7 +10,9 @@ class Project {
 	constructor() {
 		this.Nav();
 		this.Context();
-		// this.Aside();
+		this.Aside();
+		this.openSidebar();
+		this.closeSidebar();
 	}
 	Nav() {
 		mainContent.appendChild(Nav());
@@ -21,6 +23,25 @@ class Project {
 	Aside() {
 		mainDiv.appendChild(Aside());
 	}
-	ShowSidebar() {}
+	openSidebar() {
+		const showSidebar = document.getElementById("showSidebar");
+		const hideSidebar = document.getElementById("hideSidebar");
+		const sidebar = document.getElementById("aside");
+		showSidebar.addEventListener("click", () => {
+			sidebar.style.display = "grid";
+			showSidebar.style.display = "none";
+			hideSidebar.style.display = "block";
+		});
+	}
+	closeSidebar() {
+		const showSidebar = document.getElementById("showSidebar");
+		const hideSidebar = document.getElementById("hideSidebar");
+		const sidebar = document.getElementById("aside");
+		hideSidebar.addEventListener("click", () => {
+			hideSidebar.style.display = "none";
+			showSidebar.style.display = "block";
+			sidebar.style.display = "none";
+		});
+	}
 }
 new Project();
